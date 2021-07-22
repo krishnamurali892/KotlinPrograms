738ee4b73b5f5fdd793a67183ca882e16470b154
Murali Krishna Reddy <krishnamurali892@gmail.com>
2021-07-22 16:06:42 +0530

    Add files via upload

---
+++ MaxProductOf3NumbersInArray.kt
@@ -0,0 +1,51 @@
+package com.murali.kotlintest
+
+fun main(){
+    // product of 2 numbers in integer array which gives maximum value
+    // Note array may include negative numbers also. but product of 2 smallest
+    // negative numbers gives bigger positive value i,e -100 * -10 = 1000
+    // val arr1 = arrayOf(1,3,4,5)  // 3 * 4 * 5 = 60
+    // val arr2 = arrayOf(1,3,-4,5) // 1 * 3 * 5 = 15
+       val arr3 = arrayOf(1,3,-4,-5) // -4 * -5 * 3= 60(minus * minus  = plus * max)
+    println(maxProduct(arr3))
+}
+
+// here time complexity is O(n) as we are doing single iteration
+// space complexity is O(1) as we are using only four
+// extra int variables, which is almost neglisible
+fun maxProduct(arr: Array<Int>): Int{
+    if(arr.isEmpty()){
+        return 0
+    }
+    var max1 = Int.MIN_VALUE
+    var max2 = Int.MIN_VALUE
+    var max3 = Int.MIN_VALUE
+
+    var min1 = Int.MAX_VALUE
+    var min2 = Int.MAX_VALUE
+
+    for (i in arr){
+        if(i > max1){
+            max3 = max2
+            max2 = max1
+            max1 = i
+        }else if(i > max2){
+            max3 = max2
+            max2 = i
+        }else if(i > max3){
+            max3 = i
+        }
+    }
+
+    for (i in arr){
+        if(i < min1){
+            min2 = min1
+            min1 = i
+        }else if(i < min2){
+            min2 = i
+        }
+    }
+    //Math is a part of java.lang package so need of import
+    return Math.max(max1*max2*max3, min1*min2*max1)
+}
+
