a2f239cdfb7a2ccbbf09deb9bd2d515aa1865b69
Murali Krishna Reddy <krishnamurali892@gmail.com>
2021-08-13 23:48:56 +0530

    Add files via upload

---
+++ StringReverse.kt
@@ -0,0 +1,27 @@
+package com.murali.kotlintest
+
+fun main(){
+    println(strReverse("abcd")) //dcba
+    println(strReverse("abba")) //abba
+}
+
+//String reverse
+fun strReverse(str:String): String{
+    var charArr = str.toCharArray()
+    var start = 0
+    var end = charArr.size - 1
+    var temp = ' '
+    while (start < end){
+        //swap chars if not same
+        if(charArr[start] != charArr[end]){
+            temp = charArr[start]
+            charArr[start] = charArr[end]
+            charArr[end] = temp
+        }
+        start++
+        end--
+    }
+    return String(charArr)
+}
+
+
