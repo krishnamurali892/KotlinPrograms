66e2dc1a437086c3f331e79f1453812700059a8f
Murali Krishna Reddy <krishnamurali892@gmail.com>
2021-08-14 13:28:36 +0530

    Update NthFibonacciNumber.kt

--- NthFibonacciNumber.kt
+++ NthFibonacciNumber.kt
@@ -5,6 +5,21 @@ fun main(){
     println(fib(9)) //34
 }
 
+//with iterator -- more code so will go with recursion
+fun fibWithIterator(n: Int): Int{
+    var fib0 = 0
+    var fib1 = 1
+    var fibn = -1
+    var i = 2
+    while(i <= n){
+        fibn = fib0 + fib1
+        fib0 = fib1
+        fib1 = fibn
+        i++
+    }
+    return fibn
+}
+
 //nth Fibonacci number with recursion - 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
 fun fib(n: Int): Int{
     if(n == 0 || n == 1){
